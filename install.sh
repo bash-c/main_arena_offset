467d4fed2cc77334415cd4127adc907d14b26a9e
0x01f <aboultraman@gmail.com>
2018-08-20 01:07:10 +0800

    v1.3

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@ wget -q https://raw.githubusercontent.com/0x01f/main_arena_offset/master/main_ar
 sudo mv main_arena /usr/local/bin/main_arena
 sudo chmod +x /usr/local/bin/main_arena
 echo "Done! Enjoy it!"
-echo -n "Usage:"
-echo -e "\033[0;35m$\033[0m \033[01;31mmain_arena\033[0m \033[1;32myour_libc_path\033[0m"
+echo -e "Usage: \033[0;35m$\033[0m \033[01;31mmain_arena\033[0m \033[1;32myour_libc_path\033[0m"
 echo "Star it on github <https://github.com/0x01f/main_arena_offset> if you like it."
